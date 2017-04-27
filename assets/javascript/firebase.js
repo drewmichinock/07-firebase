a793f6d14376fad4d225b8812c4f686b0083e2cc
drewmichinock <drewmichinock@gmail.com>
2017-04-27 14:14:43 -0700

    2nd commit

---
+++ assets/javascript/firebase.js
@@ -0,0 +1,19 @@
+  // initialize firebase
+  var config = {
+      apiKey: "AIzaSyCGjHHj54zXIlfU8dl6USJQPD45DWlQ2Yg",
+      authDomain: "train-time-30872.firebaseapp.com",
+      databaseURL: "https://train-time-30872.firebaseio.com",
+      projectId: "train-time-30872",
+      storageBucket: "train-time-30872.appspot.com",
+      messagingSenderId: "631340263673"
+  };
+
+  firebase.initializeApp(config);
+
+  // variable created to reference database
+  var db = firebase.database();
+
+  // initial values
+  var # = #;
+  var # = #;
+  var # = #;
