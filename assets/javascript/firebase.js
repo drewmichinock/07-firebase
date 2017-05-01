fdb58f541bd070e0bf97047819d1f68b9e7ef0da
drewmichinock <drewmichinock@gmail.com>
2017-05-01 10:59:35 -0700

    6th commit

--- assets/javascript/firebase.js
+++ assets/javascript/firebase.js
@@ -11,7 +11,55 @@
   firebase.initializeApp(config);
 
   // variable created to reference database
-  var db = firebase.database();
-  var # = #;
-  var # = #;
-  var # = #;
+  var database = firebase.database();
+
+  // initiate jquery
+  $(document).ready(function() {
+
+      // when submit button clicked
+      $("#submit").on("click", function(event) {
+
+          // prevent form submission
+          event.preventDefault();
+
+          // data submission test
+          console.log("test")
+
+          // get inputs and store in variables
+          var addName = $("#input-name").val().trim();
+          var addDest = $("#input-dest").val().trim();
+          var addTime = new Date($("#input-time").val()).getTime();
+          var addFreq = $("#input-freq").val().trim();
+
+          // push inputs to firebase database
+          database.ref("/trains").push({
+
+              name: addName,
+              role: addRole,
+              start: addStart,
+              rate: addRate
+
+          });
+
+      });
+
+      // show children of employees and key in console log
+      database.ref("/employees").on("child_added", function(snapshot) {
+
+          console.log(snapshot.val());
+
+          console.log(snapshot.getKey());
+
+          var tr = $("<tr>");
+
+          var nameTD = $("<td>");
+
+          nameTD.html(snapshot.val().name)
+
+          tr.append(nameTD);
+
+          // append to table body
+
+      });
+
+  })
